0a433f564103010ee1471ed89453b77dea661b68
ch <632934215@qq.com>
2022-05-14 23:42:50 +0800

    the behavior of the dealer has been completed ; tested succesfully ; next:behaviors of share holders and KZG verification.

--- internal/party/VSS_test.go
+++ internal/party/VSS_test.go
@@ -1,9 +1,81 @@
 package party
 
 import (
+	"fmt"
+	"github.com/DyCAPSTeam/DyCAPS/pkg/protobuf"
+	"github.com/Nik-U/pbc"
+	"github.com/ncw/gmp"
+	"google.golang.org/protobuf/proto"
 	"testing"
 )
 
-func TestVSS(t *testing.T) {
-	
+func TestDealer(t *testing.T) {
+
+	ipList := []string{"127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1"}
+	portList := []string{"8880", "8881", "8882", "8883", "8884", "8885", "8886"}
+
+	N := uint32(7)
+	F := uint32(2)
+	sk, pk := SigKeyGen(N, 2*F+2)
+
+	var p []*HonestParty = make([]*HonestParty, N)
+	for i := uint32(0); i < N; i++ {
+		p[i] = NewHonestParty(N, F, i, ipList, portList, pk, sk[i])
+	}
+
+	for i := uint32(0); i < N; i++ {
+		p[i].InitReceiveChannel()
+	}
+
+	for i := uint32(0); i < N; i++ {
+		p[i].InitSendChannel()
+	}
+
+	var client Client
+	client.s = new(gmp.Int)
+	client.s.SetInt64(int64(111111111111111))
+	client.HonestParty = NewHonestParty(N, F, 0x7fffffff, ipList, portList, pk, sk[2*F+1])
+	client.InitSendChannel()
+
+	KZG.SetupFix(int(F))
+
+	client.Share([]byte("vssshare"))
+
+	for i := 0; uint32(i) < N; i++ {
+		m := <-p[i].GetMessage("VSSSend", []byte("vssshare"))
+		data := m.Data
+		var content protobuf.VSSSend
+		proto.Unmarshal(data, &content)
+		gs := new(pbc.Element)
+		gs = KZG.NewG1()
+		gs.SetCompressedBytes(content.Pi.Gs)
+
+		fmt.Println("node", i+1, "receive", m.Sender, m.Id, m.Type, m.Data)
+		fmt.Println("the gs received is", gs.String())
+		var pi_test = new(Pi)
+		pi_test.Pi_contents = make([]Pi_Content, 2*F+2)
+		for j := 0; uint32(j) <= 2*F+1; j++ {
+			pi_test.Pi_contents[j].CR_j = KZG.NewG1()
+			pi_test.Pi_contents[j].CZ_j = KZG.NewG1()
+			pi_test.Pi_contents[j].WZ_0 = KZG.NewG1()
+			pi_test.Pi_contents[j].g_Fj = KZG.NewG1()
+		}
+		for j := 1; uint32(j) <= 2*F+1; j++ {
+			pi_test.Pi_contents[j].CR_j.SetCompressedBytes(content.Pi.PiContents[j].CRJ)
+			pi_test.Pi_contents[j].CZ_j.SetCompressedBytes(content.Pi.PiContents[j].CZJ)
+			pi_test.Pi_contents[j].WZ_0.SetCompressedBytes(content.Pi.PiContents[j].WZ_0)
+			pi_test.Pi_contents[j].g_Fj.SetCompressedBytes(content.Pi.PiContents[j].G_Fj)
+			fmt.Println("j= ", j, "; CRj= ", pi_test.Pi_contents[j].CR_j.String(), "; CZj= ", pi_test.Pi_contents[j].CZ_j.String(), "; WZj_0= ", pi_test.Pi_contents[j].WZ_0.String(), "; g_Fj= ", pi_test.Pi_contents[j].g_Fj.String())
+		}
+		for j := 1; uint32(j) <= 2*F+1; j++ {
+			Rji := new(gmp.Int)
+			WRji := new(pbc.Element)
+			Rji = gmp.NewInt(0)
+			WRji = KZG.NewG1()
+			Rji.SetBytes(content.RjiList[j])
+			WRji.SetCompressedBytes(content.WRjiList[j])
+			fmt.Println("i= ", i+1, " j = ", j, " Rji = ", Rji, "WRji = ", WRji.String())
+		}
+	}
+
 }
--- internal/party/client.go
+++ internal/party/client.go
@@ -1,12 +1,14 @@
 package party
 
 import (
+	"fmt"
 	"github.com/DyCAPSTeam/DyCAPS/internal/commitment"
 	"github.com/DyCAPSTeam/DyCAPS/internal/ecparam"
 	"github.com/DyCAPSTeam/DyCAPS/internal/polyring"
 	"github.com/DyCAPSTeam/DyCAPS/pkg/protobuf"
 	"github.com/Nik-U/pbc"
 	"github.com/ncw/gmp"
+	"google.golang.org/protobuf/proto"
 	"math/rand"
 	"time"
 )
@@ -27,33 +29,37 @@ type Pi_Content struct {
 }
 
 type Pi struct {
-	g_s *pbc.Element
+	G_s *pbc.Element
 	//add pi_contents
-	pi_contents []Pi_Content
+	Pi_contents []Pi_Content
 }
 
 //假设这里KZG的初始化已经完成
 func (client *Client) Share(ID []byte) {
 
-	var pi *Pi
-	pi.pi_contents = make([]Pi_Content, 2*client.F+2) // here we do not use pi_contents[0]
-	var p = ecparam.PBC256.Ngmp                       // the primitive of Zp* (the type is *gmp.Int)
-
+	var pi *Pi = new(Pi)
+	pi.G_s = KZG.NewG1()                              //  is this OK?
+	pi.Pi_contents = make([]Pi_Content, 2*client.F+2) // here we do not use pi_contents[0]
+	var p *gmp.Int                                    // the primitive of Zp* (the type is *gmp.Int)
+	p = ecparam.PBC256.Ngmp
 	//pi <- g^s
 	s_poly, _ := polyring.New(0)
 	_ = s_poly.SetCoefficientBig(0, client.s)
-	KZG.Commit(pi.g_s, s_poly)
+	KZG.Commit(pi.G_s, s_poly)
 
 	//generate 2t degree random polynomial F , F(0) = s
 	var rnd = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 	var F, _ = polyring.NewRand(int(2*client.F), rnd, p)
 	var F_ValueAt = make([]*gmp.Int, 2*client.F+2) // here we do not use F_ValueAt[0]
+	for i := 0; uint32(i) < 2*client.F+2; i++ {
+		F_ValueAt[i] = gmp.NewInt(0)
+	}
 	F.SetCoefficientBig(0, client.s)
 
 	//generate 2t+1 t-degree Rj(x)
 	var R_list = make([]polyring.Polynomial, 2*client.F+2) // here we do not use R_list[0]
 	var rnd2 = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
-	for i := 1; uint32(0) <= 2*client.F+1; i++ {
+	for i := 1; uint32(i) <= 2*client.F+1; i++ {
 		F.EvalMod(gmp.NewInt(int64(i)), p, F_ValueAt[i])
 		R_list[i], _ = polyring.NewRand(int(client.F), rnd2, p)
 		R_list[i].SetCoefficientBig(0, F_ValueAt[i])
@@ -64,34 +70,106 @@ func (client *Client) Share(ID []byte) {
 	var C_R_list = make([]*pbc.Element, 2*client.F+2)      // here we do not use C_R_list[0]
 	var C_Z_list = make([]*pbc.Element, 2*client.F+2)      // here we do not use C_Z_list[0]
 	var W_Z0_list = make([]*pbc.Element, 2*client.F+2)
+	for i := 0; uint32(i) <= 2*client.F+1; i++ {
+		C_R_list[i] = KZG.NewG1()
+		C_Z_list[i] = KZG.NewG1()
+		W_Z0_list[i] = KZG.NewG1()
+	}
 
-	copy(Z_list, R_list)
+	for i := 1; uint32(i) <= 2*client.F+1; i++ {
+		Z_list[i] = polyring.NewEmpty()
+		Z_list[i].ResetTo(R_list[i])
+	}
+
 	for i := 1; uint32(i) <= 2*client.F+1; i++ {
 		//generate Z_list
 		temp, _ := polyring.New(0) // temp means the 0-degree polynomial f(x) = F_ValueAt[i]
 		temp.SetCoefficientBig(0, F_ValueAt[i])
-		Z_list[i].Sub(Z_list[i], temp)
+		copyedZ := polyring.NewEmpty()
+		copyedZ.ResetTo(Z_list[i])
+		Z_list[i].Sub(copyedZ, temp)
 		Z_list[i].Mod(p) // don't forget mod p!!!
-
 		//commit R_list
 		KZG.Commit(C_R_list[i], R_list[i])
 		//commit Z_list
 		KZG.Commit(C_Z_list[i], Z_list[i])
 		//create witness of (Zj(x),0)
 		KZG.CreateWitness(W_Z0_list[i], Z_list[i], gmp.NewInt(0))
+		fmt.Println("WZ0_i = ", W_Z0_list[i].CompressedBytes())
 		//add to pi
-		var FjCommit *pbc.Element
+		var FjCommit *pbc.Element = KZG.NewG1()
 		KZG.Commit(FjCommit, temp)
-		pi.pi_contents[i] = Pi_Content{j: i, CR_j: C_R_list[i], CZ_j: C_Z_list[i], WZ_0: W_Z0_list[i], g_Fj: FjCommit}
+		pi.Pi_contents[i] = Pi_Content{j: i, CR_j: C_R_list[i], CZ_j: C_Z_list[i], WZ_0: W_Z0_list[i], g_Fj: FjCommit}
 	}
 
 	//Send
 	W_Rji := make([][]*pbc.Element, client.N+1) // the first index is in range[1,N],and the second [1,2F+1]. start from 1
+	Rji_list := make([][]*gmp.Int, client.N+1)
+
+	fmt.Println("the gs in pi is: ", pi.G_s.String())
+
+	//test
+	for i := 1; uint32(i) <= 2*client.F+1; i++ {
+		fmt.Println("j= ", i, "; CRj= ", pi.Pi_contents[i].CR_j.String(), "; CZj= ", pi.Pi_contents[i].CZ_j.String(), "; WZj_0= ", pi.Pi_contents[i].WZ_0.String(), "; g_Fj= ", pi.Pi_contents[i].g_Fj.String())
+	}
 	for i := 1; uint32(i) <= client.N; i++ {
 		W_Rji[i] = make([]*pbc.Element, 2*client.F+2) // start from 1
+		Rji_list[i] = make([]*gmp.Int, 2*client.F+2)  // start from 1
+		for j := 0; uint32(j) <= 2*client.F+1; j++ {
+			Rji_list[i][j] = gmp.NewInt(0)
+			W_Rji[i][j] = KZG.NewG1()
+		}
+		//set W_Rji
 		for j := 1; uint32(j) <= 2*client.F+1; j++ {
 			KZG.CreateWitness(W_Rji[i][j], R_list[j], gmp.NewInt(int64(i)))
+			R_list[j].EvalMod(gmp.NewInt(int64(i)), p, Rji_list[i][j])
 		}
-		client.Send(&protobuf.Message{}, uint32(i)) // how to marshal the Pi type?
+		//encapsulate
+		data := Encapsulate_VSSSend(pi, Rji_list[i], W_Rji[i], client.N, client.F)
+		fmt.Println("client wants to send data to ", i-1, " : ", data)
+		for j := 1; uint32(j) <= 2*client.F+1; j++ {
+			fmt.Println("i= ", i, " j= ", j, " Rj(i) = ", Rji_list[i][j], " W_Rji = ", W_Rji[i][j])
+		}
+		client.Send(&protobuf.Message{
+			Type:   "VSSSend",
+			Id:     ID,
+			Sender: 0x7fffffff, // 0x7fffffff denote dealer
+			Data:   data,
+		}, uint32(i-1)) // pid = i - 1!
+		fmt.Println("client send VSSSend message to ", i)
 	}
+}
+
+func Encapsulate_VSSSend(pi *Pi, Rji_list []*gmp.Int, Wji_list []*pbc.Element, N uint32, F uint32) []byte {
+	var msg = new(protobuf.VSSSend)
+	msg.Pi = new(protobuf.Pi)
+	msg.Pi.Gs = pi.G_s.CompressedBytes()
+
+	for j := 0; uint32(j) <= 2*F+1; j++ {
+		if j == 0 {
+			msg.RjiList = make([][]byte, 2*F+2) // 0 is not used.
+			msg.WRjiList = make([][]byte, 2*F+2)
+			msg.WRjiList[0] = []byte{}
+			msg.RjiList[0] = []byte{}
+			msg.Pi.PiContents = make([]*protobuf.PiContent, 2*F+2)
+			for k := 0; uint32(k) <= 2*F+1; k++ {
+				msg.Pi.PiContents[k] = new(protobuf.PiContent)
+			}
+			msg.Pi.PiContents[0].J = 0
+			msg.Pi.PiContents[0].WZ_0 = []byte{}
+			msg.Pi.PiContents[0].CRJ = []byte{}
+			msg.Pi.PiContents[0].CZJ = []byte{}
+			msg.Pi.PiContents[0].G_Fj = []byte{}
+		} else {
+			msg.WRjiList[j] = Wji_list[j].CompressedBytes()
+			msg.RjiList[j] = Rji_list[j].Bytes()
+			msg.Pi.PiContents[j].J = int32(j)
+			msg.Pi.PiContents[j].CZJ = pi.Pi_contents[j].CZ_j.CompressedBytes()
+			msg.Pi.PiContents[j].CRJ = pi.Pi_contents[j].CR_j.CompressedBytes()
+			msg.Pi.PiContents[j].WZ_0 = pi.Pi_contents[j].WZ_0.CompressedBytes()
+			msg.Pi.PiContents[j].G_Fj = pi.Pi_contents[j].g_Fj.CompressedBytes()
+		}
+	}
+	data, _ := proto.Marshal(msg)
+	return data
 }
